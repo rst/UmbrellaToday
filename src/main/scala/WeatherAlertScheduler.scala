7d4d7b9f4857dfdeb91460b1440219c49c8108b2
Robert Thau <rst@ai.mit.edu>
2011-07-21 19:08:23 -0400

    Enable alerts and notifications.
    Which, mirabile dictu, all seem to be working.  (As I write, the
    web site is expecting rain tomorrow in Paris.)

--- src/main/scala/WeatherAlertScheduler.scala
+++ src/main/scala/WeatherAlertScheduler.scala
@@ -5,6 +5,7 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import android.text.format.DateFormat
 
 trait WeatherAlertScheduler 
   extends org.positronicnet.util.AppFacility
@@ -23,13 +24,20 @@ trait WeatherAlertScheduler
     val alarmManager = 
       context.getSystemService(Context.ALARM_SERVICE).asInstanceOf[AlarmManager]
 
-    if (true)
-      Log.d( "XXX", "Not setting alerts yet" )
-    else if (alert == null)
+    if (alert == null) {
+      Log.d( UmbrellaTodayApplication.logTag, "Canceling weather alerts")
       alarmManager.cancel( makePendingIntent( context, makeIntent( context )))
-    else
+    }
+    else {
+      val dateFmt = DateFormat.getDateFormat( context )
+      val timeFmt = DateFormat.getTimeFormat( context )
+      Log.d( UmbrellaTodayApplication.logTag, 
+             "Scheduling next UmbrellaToday alarm for " + 
+             dateFmt.format( alert.alertTime ) +
+             " at " + timeFmt.format( alert.alertTime ))
       alarmManager.set( AlarmManager.RTC_WAKEUP, alert.alertTime,
                         makePendingIntent(context, makeIntent(context, alert )))
+    }
   }
 
   private def makeIntent( c: Context ): Intent = 
